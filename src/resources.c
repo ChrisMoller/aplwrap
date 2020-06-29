5a8486260fba9c78487de62a7314f3f5d59e0c0f
ChrisMoller <mollerware@mollerware.com>
2020-06-29 15:50:02 -0400

    Added save/restore for font size and fg/bg colours.

--- src/resources.c
+++ src/resources.c
@@ -2,39 +2,90 @@
 
 #include <gtk/gtk.h>
 #include <glib/gi18n-lib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 
 #include "menu.h"
 #include "aplwrap.h"
+#include "options.h"
 
 #define GROUP_APLWRAP  "AplwrapResources"
+
 #define KEY_SHOW_PSTAT "ShowPstat"
+#define KEY_FONT_SIZE  "FontSize"
+#define KEY_FOREGROUND_COLOUR  "FgColour"
+#define KEY_BACKGROUND_COLOUR  "BgColour"
+
+static gchar *resource_path = NULL;
+
+static void
+build_resource_path ()
+{
+  gchar *config_path = g_strdup_printf ("%s/.config", g_get_home_dir ());
+  if (g_file_test (config_path, G_FILE_TEST_EXISTS)) {
+    if (!g_file_test (config_path, G_FILE_TEST_IS_DIR)) return;  // funky
+  }
+  else mkdir (config_path, 0775);    // ~/.config doesn't exist, create it.
+  resource_path =
+    g_strdup_printf ("%s/%s", config_path, g_get_prgname ());
+  g_free (config_path);
+}
 
 void
 restore_resources ()
 {
   GKeyFile *key_file;
   gboolean rc;
-  gchar *path;
+  GError *error = NULL;
 
-  path = g_strdup_printf ("%s/.%s",
-			  g_get_home_dir (),
-			  g_get_prgname ());
+  build_resource_path ();
 
   key_file = g_key_file_new ();
   rc = g_key_file_load_from_file (key_file,
-                                  path,
+                                  resource_path,
                                   G_KEY_FILE_KEEP_COMMENTS
                                   | G_KEY_FILE_KEEP_TRANSLATIONS,
                                   NULL);
 
   if (rc) {
     gboolean bv;
+    gint iv;
+    gchar *sv;
+
     bv = g_key_file_get_boolean (key_file,
 				 GROUP_APLWRAP,
 				 KEY_SHOW_PSTAT,
-				 NULL);
+				 &error);
+    if (error) g_clear_error (&error);
+    else show_status = bv;
 
-    show_status = bv;
+    iv = g_key_file_get_integer (key_file,
+				 GROUP_APLWRAP,
+				 KEY_FONT_SIZE,
+				 &error);
+    if (error) g_clear_error (&error);
+    else ft_size = iv;
+
+    sv = g_key_file_get_string (key_file,
+				GROUP_APLWRAP,
+				KEY_FOREGROUND_COLOUR,
+				&error);
+    if (error) g_clear_error (&error);
+    else {
+      if (fg_colour) g_free (fg_colour);
+      fg_colour = sv;
+    }
+
+    sv = g_key_file_get_string (key_file,
+				GROUP_APLWRAP,
+				KEY_BACKGROUND_COLOUR,
+				&error);
+    if (error) g_clear_error (&error);
+    else {
+      if (bg_colour) g_free (bg_colour);
+      bg_colour = sv;
+    }
   }
 
   g_key_file_free (key_file); 
@@ -58,21 +109,31 @@ save_resources ()
                           KEY_SHOW_PSTAT,
                           show_status);
 
+  g_key_file_set_string (key_file,
+			 GROUP_APLWRAP,
+			 KEY_FOREGROUND_COLOUR,
+			 fg_colour);
+
+  g_key_file_set_string (key_file,
+			 GROUP_APLWRAP,
+			 KEY_BACKGROUND_COLOUR,
+			 bg_colour);
+
+  g_key_file_set_integer (key_file,
+			  GROUP_APLWRAP,
+			  KEY_FONT_SIZE,
+			  ft_size);
+
   {
-    gchar *path;
     gchar *data = NULL;
     gsize data_length;
     
     data = g_key_file_to_data (key_file, &data_length, NULL);
-    path = g_strdup_printf ("%s/.%s",
-                            g_get_home_dir (),
-                            g_get_prgname ());
-    g_file_set_contents (path,
+    g_file_set_contents (resource_path,
                          data,
                          data_length,
                          NULL);
-    g_free (path);
-    
+
     if (data) g_free (data);
   }
 
